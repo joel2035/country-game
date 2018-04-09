d65881fc5e311ee793b8d55ae266f8e5175f9dc6
joel2035 <joel.pokam@hetic.net>
2018-04-09 17:07:46 +0200

    2em

--- app.js
+++ app.js
@@ -8,13 +8,21 @@ var time = document.querySelector('.time span')
 var gameOver = document.querySelector('#game-over')
 var button = document.querySelector('.button')
 var count = 0
+var live = document.querySelector(".lives")
+var btn2 = document.querySelector('.button')
+
 
 var number
 count = time.textContent
 var change_country = pays
+btn2.addEventListener('click', function() {
+  gameStart.classList.add('is-open');
 
+})
 btn.addEventListener('click', function() {
   gameStart.classList.remove('is-open');
+
+
   button.addEventListener('click', function() {
     gameOver.classList.remove('is-open')
     count = 21
@@ -22,11 +30,12 @@ btn.addEventListener('click', function() {
   })
 
 
+
   function set() {
     if (count > 0) {
       count--
-      time.textContent = count;
 
+      time.textContent = count;
 
 
     } else {
@@ -46,7 +55,30 @@ for (var i = 0; i < flag.length; i++) {
   number = Math.floor(Math.random() * flags.length)
   change.push(flags[number].name)
   flag[i].setAttribute('src', "flags/" + flags[number].code + ".svg")
+  flag[i].setAttribute('name', flags[number].name)
+  flag[i].addEventListener("click", function() {
+    var image = document.querySelector(".img1")
+    var test = document.querySelectorAll(".img1")
+    var j = test.length
 
+
+
+    if (this.name === pays.innerHTML) {
+      count += 3;
+
+
+    } else {
+
+      if (image !== null && j > 1) {
+        live.removeChild(image)
+
+      } else if (j === 1) {
+        gameOver.classList.add("is-open")
+
+      }
+
+    }
+  })
 
 }
 pays.innerHTML = change[Math.floor(Math.random() * change.length)]
